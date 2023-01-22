bbbb1ce9d31da3eb70326c617b7255bfe49ba0ca
7te3ep <7te3ep@gmail.com>
2023-01-22 22:21:05 +0100

    avant que je casse toi avec le globe en 3d

--- game.js
+++ game.js
@@ -1,27 +1,14 @@
 import jsonData from './world.geo.json-master/countries.geo.json' assert { type: "json" }
-console.log(d3.select("svg").select('g').selectAll('path')._groups)
 
-    var paths = document.querySelector('#map').querySelector("g").querySelectorAll('.country');
-
-
-
-    paths.forEach((path)=>{
-        path.addEventListener("click",(e)=>{
-            if (e.target.id == answer) {
-                alert("win")
-                score ++ 
-                scoreDisplay.innerHTML = score
-                generate()
-            }
-        })
-    })
-    
-
-var pays = new Array()
-jsonData.features.forEach((item)=>{
-    pays.push(item.properties.name)
-})
-
+function pathClicked(e){
+    console.log(e)
+    if (e.properties.name == answer) {
+        alert("win")
+        score ++ 
+        scoreDisplay.innerHTML = score
+        generate()
+    }
+}
 const shuffle = arr => [...arr].sort(() => 0.5 - Math.random());
 
 var answer = ""
@@ -30,6 +17,7 @@ let btn = document.getElementById('start')
 let scoreDisplay = document.getElementById('score')
 let skip = document.getElementById('skip')
 let score = 0
+
 btn.addEventListener('click',(e)=>{
     startGame()
     generate()
@@ -37,6 +25,11 @@ btn.addEventListener('click',(e)=>{
 
 skip.addEventListener('click',(e)=>{
     generate()
+})
+
+var pays = new Array()
+jsonData.features.forEach((item)=>{
+    pays.push(item.properties.name)
 })
 
 function generate() {
@@ -50,6 +43,7 @@ function startGame(){
     scoreDisplay.innerHTML = score
 }
 
+export {pathClicked}
 
 
 
